8c023099292d0ed3678f794e2c18c009cf499237
Ashish Sheoran <sheoranashu079@gmail.com>
2021-03-30 00:32:28 -0400

    Added JS for sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
-    
-    
+    // For the sticky Navigation
+    $('.js--section-features').waypoint(function(direction) {
+        if(direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+
 })
